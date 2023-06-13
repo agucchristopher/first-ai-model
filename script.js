e705b76559ef0ba20bef9c94394f85db7b1e9d2c
Agu C. Christopher <aguchris740@gmail.com>
2023-06-13 16:34:28 -0700

    Added files

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 const tf = require("@tensorflow/tfjs");
 
-const model = tf.sequential();
-model.add(tf.layers.dense({ units: 1, inputShape: [2] }));
+try {
+  const model = tf.sequential();
+  model.add(tf.layers.dense({ units: 1, inputShape: [2] }));
 
-model.compile({ loss: "meanSquaredErro", optimizer: "sgd" });
+  model.compile({ loss: "meanSquaredErro", optimizer: "sgd" });
 
-const input = tf.tensor([
-  [0, 0],
-  [0, 1],
-  [1, 0],
-  [1, 1],
-]);
-const output = tf.tensor([[0], [1], [1], [2]]);
-
-model.fit(input, output, { epochs: 10 }).then(() => {
-  const testData = tf.tensor([
-    [0, 1],
+  const input = tf.tensor([
     [1, 1],
-    [1, 0],
+    [0, 0],
   ]);
-  const predictions = model.predict(testData);
-  predictions.print();
-});
+  const output = tf.tensor([[0], [1]]);
+
+  model.fit(input, output, { epochs: 10 }).then(() => {
+    const testData = tf.tensor([
+      [1, 1],
+      [0, 0],
+    ]);
+    const predictions = model.predict(testData);
+    predictions.print();
+  });
+} catch (error) {
+  console.log(error.message);
+}
